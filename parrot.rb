676964e68737d90ad8a73fb60cd0fb375dd65ae6
Ashley Sigler <ashleysigler13@gmail.com>
2020-07-07 21:03:19 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,4 +1,5 @@
-def parrot (phrase = "Squawk")
-  puts phrase
-  return ("Squawk!")
+def parrot (phrase = "Pretty bird!")
+  puts ("Pretty bird #{phrase}!")
+  puts phrase = ("Squawk!")
+  return phrase
 end
